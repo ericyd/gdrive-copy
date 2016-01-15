36f390833c7c66bfd522d580147de730941bbc6f
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-14 21:41:24 -0800

    incorporated Sass into css and minification into js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var concat = require('gulp-concat');
-var cssmin = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var insert = require('gulp-insert');
 
 gulp.task('default', function(){
     // Default task
@@ -17,7 +18,9 @@ gulp.task('js', function() {
     gulp.src('./src/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        .pipe(concat('JavaScript.html'))
+        .pipe(uglify())
+        .pipe(concat('js.html'))
+        .pipe(insert.wrap('<script>', '</script>'))
         .pipe(gulp.dest('dist'));
         
     gulp.src('./src/Code.gs')
@@ -28,10 +31,11 @@ gulp.task('js', function() {
 
 gulp.task('css', function() {
     // process css
-    gulp.src('./src/css/main')
-        .pipe(cssmin())
-        .pipe(concat('Stylesheet.html'))
-        .pipe(gulp.dest('dist'));
+    gulp.src('./src/css/main.css')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(concat('css.html'))
+        .pipe(insert.wrap('<style>', '</style>'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('html', function() {
