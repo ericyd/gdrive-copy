4f5e1c7f79281764d76c7661e872221163d3aa47
Eric Dauenhauer <eric_yd@yahoo.com>
2016-01-30 14:52:07 -0800

    remove folderId parsing (done automatically with picker); pass selectedFolder object to google.script.run.getValues()

--- src/js/getValues.js
+++ src/js/getValues.js
@@ -10,36 +10,9 @@
 var $ = jQuery = require('jquery');
 var createFolders = require('./createFolders.js');
 
-exports.run = function () {
+exports.run = function (selectedFolder) {
   
   var folderTree = [];
- 
-  // Set a temporary variable to the value passed into the "folderId" field
-  var fId = thisForm.folderId.value;
-  
-  // Get the index of the string at which the folderId starts
-  var idStart = fId.search("id=");
-  var foldersStart = fId.search("folders");
-  if (idStart > 0) {
-    // Slice the string starting 3 indices after "id=", which means that it takes away "id=" and leaves the rest
-    fId = fId.slice(idStart+3);  
-  } else if (foldersStart > 0) {
-    fId = fId.slice(foldersStart + 8);  
-  }
-  
-  
-  // Find the ampersand in the remaining string, which is the delimiter between the folderId and the sharing privileges
-  var amp = fId.indexOf("&");
-  
-  // Slice the string up to the ampersand
-  if (amp > 0) {
-    fId = fId.slice(0,amp);
-  }
-  
-  // Set the folderId element within thisForm (retrieved from doGet) to the new, sliced fId variable
-  thisForm.folderId.value = fId;
-  
-  
   
   // Get values of folder Ids and pass them to other functions
   return google.script.run
@@ -57,7 +30,7 @@ exports.run = function () {
     // Respond to failure conditions here.
     $("#errors").append("<div class='alert alert-danger' role='alert'><b>Error:</b> There was an error getting the folder information.<br />" + msg + ".<br />Please make sure you are using Google Chrome or Chromium.</div>");
   })
-  .getValues( thisForm );
+  .getValues( thisForm, selectedFolder );
   
   
 }
