368187a048292d6318beea28a62b6e74121361e6
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-21 16:04:15 -0800

    new copy function serves basic json to logger spreadsheet

--- src/gs/copy.js
+++ src/gs/copy.js
@@ -1,78 +1,64 @@
 /**
- * Copies files from a single source folder to a single
- * destination folder.  Optionally copies sharing permissions
- * associated with original file
- *
- * @param {string} folderId identifier of source folder
- * @param {string} newFolderId identifier of destination folder
- * @param {boolean} copyPermissions copy permissions associated with source 
- *    folder files to destination folder files
- * @param {array} added list of email addresses already added to parent
- *    folders and thus do not need to be duplicated, thus reducing number
- *    of emails sent 
+ * Copy folders and files from source to destination.
+ * Get parameters from userProperties,
+ * Loop until time runs out,
+ * then call timeout methods, saveProperties and createTrigger.
  */
-function copyFiles(folderId, newFolderId, copyPermissions, added) {
-  var fromFolder = DriveApp.getFolderById(folderId);
-  var toFolder = DriveApp.getFolderById(newFolderId);
-  var toFolderName = toFolder.getName();
-  var files = fromFolder.getFiles();
-
-  while (files.hasNext()) {
-    var file = files.next();
-    var filename = file.getName();
-    var newFile = file.makeCopy(filename, toFolder);
+function copy() {
+    Logger.log("copy called");
     
-    // Copy Permissions if option is selected
-    if (copyPermissions) {
-      var oldFileEditors = file.getEditors();
-      oldFileEditors.push(file.getOwner());
-      var oldFileViewers = file.getViewers();
-      
-      // If file is a Google Doc or Spreadsheet,
-      // then use the DocumentApp or SpreadsheetApp to add editors/owners.  
-      // This suppresses unnessessary emails
-      if (file.getMimeType() == "application/vnd.google-apps.document") {
-        var newDoc = DocumentApp.openById(newFile.getId());
-        for (i = 0; i < oldFileEditors.length; i++) {
-          if (added.indexOf(oldFileEditors[i].getEmail()) == -1) {
-            newDoc.addEditor(oldFileEditors[i]);
-          }
+    var timeIsUp,       // {boolean} true if max execution time is reached while executing script
+        permissions,    // {boolean} true if sharing permissions should be copied from source to destination
+        currTime,       // {number} integer representing current time in milliseconds
+        copyObject,     // {object} all the files and folders to copy
+        srcId,          // {string} identification for top level source folder
+        destId,         // {string} identification for top level destination folder
+        ss,             // {object} instance of Sheet class
+        properties,     // {object} properties of current run
+        userProperties = PropertiesService.getUserProperties(); // {object} instance of Properties class
+        
+    properties = userProperties.getProperties();
+    
+    
+    // get child folders from srcId
+    var query = '"' + properties.srcId + '" in parents and trashed = false and ' +
+        'mimeType = "application/vnd.google-apps.folder"';
+        
+    var folders, pageToken;
+    
+    folders = Drive.Files.list({
+        q: query,
+        maxResults: 1000,
+        pageToken: pageToken
+        //fields: items(id,kind,ownedByMe,owners,parents,permissions,title,userPermission,writersCanShare),kind,nextPageToken
+    });
+    
+        
+    
+    var MAX_RUNNING_TIME = 5.7 * 60 * 1000;   // 5.7 minutes in milliseconds
+    var START_TIME = (new Date()).getTime();
+    
+    for (var key in properties) {
+        Logger.log('key: ' + key + ", value: " + properties[key]);
+    }
+    
+    ss = SpreadsheetApp.openById(properties.spreadsheetId).getSheetByName("Log");
+    
+    // if no current objects exist to copy, get new ones
+    if ( folders.items.length > 0 ) {
+        
+        for (var i = 0; i < folders.items.length; i ++) {
+            //sheet.getRange(row, column, numRows, numColumns)
+             //ss.getRange(2+i, 1, 1, 1).setValue(properties.srcChildFolders.items[i].id);
+             ss.getRange(2+i, 1, 1, 1).setValue(i);
+             ss.getRange(2+i, 2, 1, 1).setValue(folders.items[i].id);
+             
         }
-        for (i = 0; i < oldFileViewers.length; i++) {
-          if (added.indexOf(oldFileViewers[i].getEmail()) == -1) {
-            newDoc.addViewer(oldFileViewers[i]);
-          }
-        }
-      // Case: Spreadsheet  
-      } else if (file.getMimeType() == "application/vnd.google-apps.spreadsheet") {
-        var newSheet = SpreadsheetApp.openById(newFile.getId());
-        for (i = 0; i < oldFileEditors.length; i++) {
-          if (added.indexOf(oldFileEditors[i].getEmail()) == -1) {
-            newSheet.addEditor(oldFileEditors[i]);
-          }
-        }
-        for (i = 0; i < oldFileViewers.length; i++) {
-          if (added.indexOf(oldFileViewers[i].getEmail()) == -1) {
-            newSheet.addViewer(oldFileViewers[i]);
-          }
-        }
-      // Case: All other files
-      } else {
-        for (i = 0; i < oldFileEditors.length; i++) {
-          if (added.indexOf(oldFileEditors[i].getEmail()) == -1) {
-            newFile.addEditor(oldFileEditors[i]);
-          }
-        }
-        for (i = 0; i < oldFileViewers.length; i++) {
-          if (added.indexOf(oldFileViewers[i].getEmail()) == -1) {
-            newFile.addViewer(oldFileViewers[i]);
-          }
-        }
-      }
-    }  
-  } // while (files.hasNext()) 
-
-  return;
+        
+    }
+    
+    return;
+    
 }
 
 
@@ -81,3 +67,53 @@ function copyFiles(folderId, newFolderId, copyPermissions, added) {
 
 
 
+
+
+
+
+
+
+/**
+ * copy permissions from source to destination file/folder
+ * 
+ * @param {string} srcId identification string for the source folder
+ * @param {string} destId identification string for the destination folder
+ * @param {string} filetype options: "file" or "folder", so know how to retrieve editors and viewers
+     Maybe unnecessary since folders are treated as files in Advanced Drive API?     
+ */
+function copyPermissions(srcId, destId, filetype) {
+    var editors = 0;// get editors
+    var viewers = 0;// get editors
+    var owners = 0; // add owners to editors
+    
+    // these need adjustment based on my variable declarations
+    if (editors.length > 0) {
+        for (var i = 0; i < editorsemails.length; i++) {
+            Drive.Permissions.insert(
+                {
+                    'role': 'writer',
+                    'type': 'user',
+                    'value': editorsemails[i]
+                },
+                destId,
+                {
+                    'sendNotificationEmails': 'false'
+                });
+        }
+    }
+
+    if (viewers.length > 0) {
+        for (var i = 0; i < viewersemails.length; i++) {
+            Drive.Permissions.insert(
+                {
+                    'role': 'reader',
+                    'type': 'user',
+                    'value': viewersemails[i]
+                },
+                destId, 
+                {
+                    'sendNotificationEmails': 'false'
+                });
+        }
+    }
+}
