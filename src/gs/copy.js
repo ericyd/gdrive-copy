cbdee2b7f74bf6c09aae1617124282e61d922c5f
Eric Dauenhauer <eric_yd@yahoo.com>
2016-04-13 13:13:39 -0700

    adding copyPermissions

--- src/gs/copy.js
+++ src/gs/copy.js
@@ -127,6 +127,14 @@ function copy() {
             
             
             
+            // Copy permissions if selected, and if permissions exist to copy
+            // if (properties.permissions && item.permissions && newfile.id) { 
+            // if (properties.permissions && item.permissions) {
+            //     copyPermissions(item.permissions, item.owners, newfile.id);
+            // }
+            
+            
+            
             // report success or failure on spreadsheet log
             if (newfile.id) {
                 // syntax: sheet.getRange(row, column, numRows, numColumns) 
@@ -225,6 +233,74 @@ function copy() {
             Logger.log(err.message);
             return err;   
         }        
+    }
+
+
+
+
+
+    /**
+     * copy permissions from source to destination file/folder
+     * 
+     * @param {string} src metadata for the source folder
+     * @param {string} dest metadata for the destination folder   
+     */
+    function copyPermissions(permissions, owners, destId) {
+        // var permissions = src.permissions;
+        // var owners = src.owners;
+        var i;
+        
+        // copy editors, viewers, and commenters from src file to dest file
+        if (permissions && permissions.length > 0){
+            for (i = 0; i < permissions.length; i++) {
+                
+                // if there is no email address, it is only sharable by link.
+                // These permissions will not include an email address, but they will include an ID
+                // Permissions.insert requests must include either value or id, 
+                // thus the need to differentiate between permission types
+                if (permissions[i].emailAddress) {
+                    Drive.Permissions.insert(
+                        {
+                            "role": permissions[i].role,
+                            "type": permissions[i].type,
+                            "value": permissions[i].emailAddress
+                        },
+                        destId,
+                        {
+                            'sendNotificationEmails': 'false'
+                        });
+                } else {
+                    Drive.Permissions.insert(
+                        {
+                            "role": permissions[i].role,
+                            "type": permissions[i].type,
+                            "id": permissions[i].id,
+                            "withLink": permissions[i].withLink
+                        },
+                        destId,
+                        {
+                            'sendNotificationEmails': 'false'
+                        });
+                }
+            }
+        }
+        
+        
+        // convert old owners to editors
+        if (owners && owners.length > 0){
+            for (i = 0; i < owners.length; i++) {
+                Drive.Permissions.insert(
+                    {
+                        "role": "user",
+                        "type": "writer",
+                        "value": owners[i].emailAddress,
+                    },
+                    destId,
+                    {
+                        'sendNotificationEmails': 'false'
+                    });
+            }
+        }
     }
 
 
