4cce054d57ad784def36795a71cb3c2a6fdd28e6
Eric Dauenhauer <eric_yd@yahoo.com>
2016-06-15 23:12:08 -0700

    fixed form toggle to show/hide correct content instead of simple toggle

--- src/js/basic-bindings.js
+++ src/js/basic-bindings.js
@@ -21,10 +21,21 @@ $(".selectOtherFolder").click(function() {
  * Show 'resume' form when #resume-button is selected.
  * Show original form when #new-copy-button is selected.
  */
-$(".toggle-forms").click(function () {
-    $("#formDiv").toggle();
-    $("#resume-form-div").toggle();
-    $(".description").toggle();
+$(".toggle-forms").click(function (e) {
+    // #new-copy-folder button is pressed
+    if (e.target.id == document.getElementsByClassName('toggle-forms').item(0).id) {
+        $("#formDiv").show();
+        $("#resume-form-div").hide();
+        $(".description").eq(0).show();
+        $(".description").eq(1).hide();    
+    }
+    // #resume-copy-folder button is pressed 
+    else {
+        $("#formDiv").hide();
+        $("#resume-form-div").show();
+        $(".description").eq(0).hide();
+        $(".description").eq(1).show();
+    }
 });
 
 
