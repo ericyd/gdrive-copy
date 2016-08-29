2b22ec725085f5239e9ad5e9edd0aa241ed188e1
Eric Dauenhauer <eric_yd@yahoo.com>
2016-08-28 18:17:41 -0700

    clean up variable declaration and add docs

--- src/gs/copy-main.js
+++ src/gs/copy-main.js
@@ -7,26 +7,21 @@
  * @param {boolean} resuming whether or not the copy call is resuming an existing folder copy or starting fresh
  */
 function copy() {
-    // CONSTANTS
-    var MAX_RUNNING_TIME = 4.7 * 60000; // return millisecs
-    var START_TIME = (new Date()).getTime();
+    /*****************************
+     * Initialize timers, initialize variables for script, and update current time
+     */
+    timers.initialize(); // global
+    //properties is a global object now, stored in the file propertiesObject
     
-    // TODO: clean up variable declarations
-    var timeIsUp,       // {boolean} true if max execution time is reached while executing script
-        currTime,       // {number} integer representing current time in milliseconds
-        ss,             // {object} instance of Sheet class
-        //properties is a global object now, stored in the file propertiesObject
+    var ss,             // {object} instance of Sheet class
         query,          // {string} query to generate Files list
-        files,          // {object} list of files within Drive folder
-        item,           // {object} metadata of child item from current iteration
+        fileList,       // {object} list of files within Drive folder
         currFolder,     // {object} metadata of folder whose children are currently being processed
-        newfile,        // {Object} JSON metadata for newly created folder or file
         timeZone,       // {string} time zone of user
-        stop,           // {boolean} true if the user has clicked the 'stop' button
         userProperties = PropertiesService.getUserProperties(), // reference to userProperties store 
         triggerId = userProperties.getProperties().triggerId;      // {string} Unique ID for the most recently created trigger
 
-    stop = userProperties.getProperties().stop == 'true';
+    timers.update(userProperties);
 
 
 
@@ -52,11 +47,8 @@ function copy() {
      * set a trigger to retry in 6 minutes.
      */
     try {
-        // Load properties and initialize logger spreadsheet
         properties = exponentialBackoff(loadProperties, 'Error restarting script, trying again...');
-
     } catch (err) {
-
         var n = Number(userProperties.getProperties().trials);
         Logger.log(n);
 
@@ -85,86 +77,93 @@ function copy() {
     
 
     /*****************************
-     * Handle leftovers from prior run
+     * Process leftover files from prior query results
+     * that weren't processed before script timed out.
+     * Destination folder must be set to the parent of the first leftover item.
+     * The list of leftover items is an equivalent array to fileList returned from the getFiles() query
      */
-
-    // TODO: Make a separate function processLeftovers() or something like that
-
-    // get current children, or skip if none exist
-    if ( properties.leftovers.items && properties.leftovers.items.length > 0) {
+    if (properties.leftovers.items && properties.leftovers.items.length > 0) {
         properties.destFolder = properties.leftovers.items[0].parents[0].id;
-        processFileList(properties.leftovers.items, timeZone, properties.permissions, userProperties, START_TIME, MAX_RUNNING_TIME, properties.map);    
+        processFileList(properties.leftovers.items, timeZone, properties.permissions, userProperties, timers, properties.map);    
     } 
     
+
+
+
+    /*****************************
+     * Update current runtime and user stop flag
+     */
+    timers.update(userProperties);
+
+
+
     
     /*****************************
-     * Query the next folder from properties.remaining
-     */
-    // When leftovers are complete, query next folder from properties.remaining
-    Logger.log("beginning processFiles on next remaining folder");    
-    while ( properties.remaining.length > 0 && !timeIsUp && !stop) {
-        
-        try {
-            // if pages remained in the previous query, use them first
-            if ( properties.pageToken ) {
-                currFolder = properties.destFolder;
-            } else {
-                currFolder = properties.remaining.shift();
-            }
-        } catch (err) {
-            log(ss, [err.message, err.fileName, err.lineNumber, Utilities.formatDate(new Date(), timeZone, "MM-dd-yy hh:mm:ss aaa")]);
+     * When leftovers are complete, query next folder from properties.remaining
+     */     
+    while (properties.remaining.length > 0 && !timers.timeIsUp && !timers.stop) {
+        // if pages remained in the previous query, use them first
+        if (properties.pageToken) {
+            currFolder = properties.destFolder;
+        } else {
+            currFolder = properties.remaining.shift();
         }
-
+        
         
         
         // build query
         query = '"' + currFolder + '" in parents and trashed = false';
         
         
-        // Get fileList using query. 
+        // Query Drive to get the fileList (children) of the current folder, currFolder
         // Repeat if pageToken exists (i.e. more than 1000 results return from the query)
         do {
 
             try {
-                // Get fileList
-                files = getFiles(query, properties.pageToken);
+                fileList = getFiles(query, properties.pageToken);
             } catch (err) {
                 log(ss, [err.message, err.fileName, err.lineNumber]);
             }
 
-            // Send items to processFileList() to copy
-            if (files.items && files.items.length > 0) {
-                processFileList(files.items, timeZone, properties.permissions, userProperties, START_TIME, MAX_RUNNING_TIME, properties.map);
+            // Send items to processFileList() to copy if there is anything to copy
+            if (fileList.items && fileList.items.length > 0) {
+                processFileList(fileList.items, timeZone, properties.permissions, userProperties, START_TIME, MAX_RUNNING_TIME, properties.map);
             } else {
                 Logger.log('No children found.');
             }
             
             // get next page token to continue iteration
-            properties.pageToken = files.nextPageToken;
+            properties.pageToken = fileList.nextPageToken;
             
-        } while (properties.pageToken && !timeIsUp && !stop);
+            timers.update(userProperties);
+
+        } while (properties.pageToken && !timers.timeIsUp && !timers.stop);
         
     }
     
-    // TODO: Delete trigger if script is complete, or if "stop" is selected
-    
-    // TODO: Pass log message to saveState() and have it log from within the function
-    if (stop) {
-        saveState('notrigger');
-        log(ss, ["Stopped manually by user.  Please use 'Resume' button to restart copying"]);
-        return;
-    }
-    
-    // If timeIsUp, maximum execution time has been reached
-    // Update logger spreadsheet, and save current items to properties.leftovers
-    if ( timeIsUp ) {
-        saveState();     
-        log(ss, ["Paused due to Google quota limits - copy will resume in 1-2 minutes"]);
-    } else {
-        // If script reaches here and !timeIsUp, then the copy is complete!  
-        // Delete prior trigger, move propertiesDoc to trash, and update logger spreadsheet,
-         
 
+
+
+    /*****************************
+     * Cleanup
+     */     
+    // Case: user manually stopped script
+    if (timers.stop) {
+        saveState(fileList, "Stopped manually by user.  Please use 'Resume' button to restart copying");
+        deleteTrigger(userProperties.getProperties().triggerId);
+        return;
+
+    // Case: maximum execution time has been reached
+    } else if (timers.timeIsUp) {
+        saveState(fileList, "Paused due to Google quota limits - copy will resume in 1-2 minutes");
+
+    // Case: the copy is complete!    
+    } else {  
+        // Delete trigger created at beginning of script, 
+        // move propertiesDoc to trash, 
+        // and update logger spreadsheet
+         
+        deleteTrigger(userProperties.getProperties().triggerId);
         try {
             Drive.Files.update({"labels": {"trashed": true}},properties.propertiesDocId);
         } catch (err) {
