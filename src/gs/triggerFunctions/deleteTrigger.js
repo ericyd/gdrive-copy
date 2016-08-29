92c115caefa9ba7654f90d2106c2eab092b68477
Eric Dauenhauer <eric_yd@yahoo.com>
2016-08-28 17:18:47 -0700

    refactored undefined triggerId check from copy

--- src/gs/triggerFunctions/deleteTrigger.js
+++ src/gs/triggerFunctions/deleteTrigger.js
@@ -5,18 +5,19 @@
  * @param {string} triggerId unique identifier for active trigger
  */
 function deleteTrigger(triggerId) {
-    try {
-        // Loop over all triggers.
-        var allTriggers = ScriptApp.getProjectTriggers();
-        for (var i = 0; i < allTriggers.length; i++) {
-            // If the current trigger is the correct one, delete it.
-            if (allTriggers[i].getUniqueId() == triggerId) {
-                ScriptApp.deleteTrigger(allTriggers[i]);
-                break;
+    if ( triggerId !== undefined && triggerId !== null) {
+        try {
+            // Loop over all triggers.
+            var allTriggers = ScriptApp.getProjectTriggers();
+            for (var i = 0; i < allTriggers.length; i++) {
+                // If the current trigger is the correct one, delete it.
+                if (allTriggers[i].getUniqueId() == triggerId) {
+                    ScriptApp.deleteTrigger(allTriggers[i]);
+                    break;
+                }
             }
+        } catch (err) {
+            log(ss, [err.message, err.fileName, err.lineNumber);
         }
-    } catch (err) {
-        log(ss, [err.message, err.fileName, err.lineNumber]);
     }
-    
 }
