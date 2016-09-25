f845eb8b383c0d7c88055f3c06367550468d59c9
ericyd <eric_yd@yahoo.com>
2016-09-24 18:26:23 -0700

    refactor module.exports notation

--- src/js/textbox-handlers.js
+++ src/js/textbox-handlers.js
@@ -14,64 +14,59 @@ var parseId = require('./parseId');
 // interval is the handle for the interval set in handleMouse and cleared in getFileData 
 var interval;
 
-// TODO: refactor these functions to live in the 'module.exports' object.
-
-/**
- * If right-click, setTimeout
- * If left-click, trigger immediately
- */
-function handleMouse(e) {
-    // if context menu is activated, give user time to paste data via context menu
-    if (e.button === 2) {
-        var g = function () {
-            return getFileData(e);
-        }; 
-        interval = setInterval(g, 500);
-        return;
-    }
-    getFileData(e);
-}
-
-
-
-/**
- * If folder URL is added, get folder metadata and display relevant information.
- * 
- * @param {object} e event object
- */
-function getFileData(e) {
-    
-    if (e.target.value !== "") {
-        DOM.onFolderLookup();
-        clearInterval(interval);
-        
-        var id = parseId( e.target.value );
-        
-        google.script.run 
-            .withSuccessHandler(function (metadata) {
-                // save metadata to picker.folder
-                picker.setSelectedFolder({
-                    "srcId": metadata.id,
-                    "srcParentId": metadata.parents[0].id,
-                    "srcName": metadata.title,
-                    "destName": "Copy of " + metadata.title
-                });
-
-            })
-            .withFailureHandler(function (msg) {
-                $(".folderSelect").hide();
-                $(".folderLookup").hide();
-                $(".selectedFolderInfo").show();
-                $(".getFolderErrors").text("Error: " + msg + "<br>You may not have permission to copy this folder.").show();
-            })
-            .getMetadata(id);
-    }
-    return false;
-    
-}
-
-
 module.exports = {
-    handleMouse: handleMouse,
-    getFileData: getFileData
+    /**
+     * If right-click, setTimeout
+     * If left-click, trigger immediately
+     */
+    'handleMouse': function(e) {
+        // if context menu is activated, give user time to paste data via context menu
+        if (e.button === 2) {
+            var g = function () {
+                return module.exports.getFileData(e);
+            }; 
+            interval = setInterval(g, 500);
+            return;
+        }
+        module.exports.getFileData(e);
+    },
+
+
+    
+
+    /**
+     * If folder URL is added, get folder metadata and display relevant information.
+     * 
+     * @param {object} e event object
+     */
+    'getFileData': function(e) {
+        
+        if (e.target.value !== "") {
+            DOM.onFolderLookup();
+            clearInterval(interval);
+            
+            var id = parseId( e.target.value );
+            
+            google.script.run 
+                .withSuccessHandler(function (metadata) {
+                    // save metadata to picker.folder
+                    picker.setSelectedFolder({
+                        "srcId": metadata.id,
+                        "srcParentId": metadata.parents[0].id,
+                        "srcName": metadata.title,
+                        "destName": "Copy of " + metadata.title
+                    });
+
+                })
+                .withFailureHandler(function (msg) {
+                    $(".folderSelect").hide();
+                    $(".folderLookup").hide();
+                    $(".selectedFolderInfo").show();
+                    $(".getFolderErrors").text("Error: " + msg + "<br>You may not have permission to copy this folder.").show();
+                })
+                .getMetadata(id);
+        }
+        return false;
+        
+    }
 }
