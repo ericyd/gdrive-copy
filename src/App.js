8355f5c2763f515d8cbf456a7bb66715a92459d6
Eric <eric@ericyd.com>
2017-11-29 21:32:13 -0800

    Update app to utilize material-ui tabs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 'use strict';
 
 import React from 'react';
-import Nav from './components/Nav';
-import ViewContainer from './components/ViewContainer';
 import { getScript, onApiLoad } from './util/picker';
 import Start from './views/Start';
 import Resume from './views/Resume';
 import Pause from './views/Pause';
 import About from './views/About';
 import FAQ from './views/FAQ';
+import AccountSwitcher from './components/AccountSwitcher';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Tabs, Tab } from 'material-ui/Tabs';
 
 export default class App extends React.Component {
   constructor() {
     super();
-
-    this.state = {
-      view: 'Start',
-      test: 'testing'
-    };
-
-    this.pickerBuilder;
-
-    this.handleViewChange = this.handleViewChange.bind(this);
-  }
-
-  handleViewChange(e) {
-    this.setState({
-      view: e.target.name
-    });
   }
 
   /**
@@ -43,27 +28,37 @@ export default class App extends React.Component {
   }
 
   render() {
+    const tabLabelStyle = { color: '#383838' };
     return (
       <MuiThemeProvider>
-        <div className="App">
-          {/* Show "test mode" if not in development */}
-          {process.env.NODE_ENV !== 'production' && (
-            <div className="test-mode-banner">TEST MODE</div>
-          )}
-          <Nav
-            handleViewChange={this.handleViewChange}
-            view={this.state.view}
-          />
-          <div className="container">
-            <ViewContainer view={this.state.view}>
-              <About viewName="About" />
-              <Start viewName="Start" pickerBuilder={this.pickerBuilder} />
-              <Resume viewName="Resume" pickerBuilder={this.pickerBuilder} />
-              <Pause viewName="Pause" />
-              <FAQ viewName="FAQ" />
-            </ViewContainer>
-          </div>
-        </div>
+        <Tabs
+          // this is what keeps everything centered
+          // if I want the bar to span and the contents to be contained, I can use the
+          // contentContainerClassName="container" property
+          className="container"
+          tabItemContainerStyle={{ backgroundColor: '#f7f7f7' }}
+          initialSelectedIndex={1}
+        >
+          <Tab label="About" style={tabLabelStyle}>
+            <About />
+          </Tab>
+
+          <Tab label="Start" style={tabLabelStyle}>
+            <Start pickerBuilder={this.pickerBuilder} />
+          </Tab>
+
+          <Tab label="Resume" style={tabLabelStyle}>
+            <Resume pickerBuilder={this.pickerBuilder} />
+          </Tab>
+
+          <Tab label="Pause" style={tabLabelStyle}>
+            <Pause />
+          </Tab>
+
+          <Tab label="FAQ" style={tabLabelStyle}>
+            <FAQ />
+          </Tab>
+        </Tabs>
       </MuiThemeProvider>
     );
   }
