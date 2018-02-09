872cb568717bacd2c50439065012566de82eb44a
Eric <eric@ericyd.com>
2018-02-08 21:23:14 -0800

    update link text

--- src/components/FolderLink.js
+++ src/components/FolderLink.js
@@ -8,7 +8,7 @@ const getDriveFolderURL = id => `https://drive.google.com/drive/folders/${id}`;
 export default function FolderLink(props) {
   return (
     <a href={getDriveFolderURL(props.folderID)} target="_blank">
-      props.name
+      {props.name}
     </a>
   );
 }
