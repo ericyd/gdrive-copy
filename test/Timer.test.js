3887ca7f0005a3d158993975c808ba5863a5cdaa
Eric <eric@ericyd.com>
2018-02-18 22:44:46 -0800

    update variable initialization and timer tests

--- test/Timer.test.js
+++ test/Timer.test.js
@@ -1,14 +1,22 @@
 const Timer = require('../lib/Timer');
-var assert = require('assert');
+const assert = require('assert');
+const sinon = require('sinon');
+const PropertiesService = require('./mocks/PropertiesService');
 
 describe('timers', function() {
   beforeEach(function() {
+    this.now = new Date().getTime();
+    this.clock = sinon.useFakeTimers(this.now);
     this.timer = new Timer();
+    this.userProperties = PropertiesService.getUserProperties();
+  });
+  afterEach(function() {
+    this.clock.restore();
   });
 
   describe('timers.START_TIME', function() {
     it('should update when initialized', function() {
-      assert.notEqual(this.timer.START_TIME, 0);
+      assert.equal(this.timer.START_TIME, this.now);
     });
   });
 
@@ -18,17 +26,24 @@ describe('timers', function() {
     });
     it('should be false when currTime is less than 4.7 minutes after START_TIME', function() {
       assert.equal(this.timer.timeIsUp, false);
-      this.timer.currTime = this.timer.START_TIME + 4.6 * 1000 * 60;
+      this.clock.tick(4.6 * 1000 * 60);
+      // this.timer.currTime = this.timer.START_TIME + 4.6 * 1000 * 60;
+      this.timer.update(this.userProperties);
       assert.equal(this.timer.timeIsUp, false);
+      assert(this.timer.canContinue(), 'timer should be able to continue');
     });
 
     it('should be true when currTime is greater than 4.7 minutes after START_TIME', function() {
       assert.equal(this.timer.timeIsUp, false);
-      this.timer.currTime = this.timer.START_TIME + 4.8 * 1000 * 60;
+      // this.timer.currTime = this.timer.START_TIME + 4.8 * 1000 * 60;
+      this.clock.tick(4.8 * 1000 * 60);
+      // this.timer.currTime = this.timer.START_TIME + 4.6 * 1000 * 60;
+      this.timer.update(this.userProperties);
       assert(
         this.timer.currTime - this.timer.START_TIME >=
           this.timer.MAX_RUNNING_TIME
       );
+      assert(!this.timer.canContinue(), 'timer should not be able to continue');
     });
   });
 });
