4e1096f7896517a9f280e5b2b1f95946bec46d09
Eric <eric@ericyd.com>
2018-02-18 19:52:06 -0800

    update error handling and bind loadProperties to properties

--- test/Properties.test.js
+++ test/Properties.test.js
@@ -1,10 +1,16 @@
 var Properties = require('../lib/Properties');
-var PropertiesService = require('./mocks/PropertiesService');
+// var PropertiesService = require('./mocks/PropertiesService');
 var assert = require('assert');
 
 describe('Properties', function() {
-  it('should be importable', function() {
-    var service = new Properties(PropertiesService);
-    assert(true);
+  describe('loadProperties()', function() {
+    xit('should get the properties document', function() {});
+    xit('should assign properties to `this`', function() {});
+    xit('should return human readable error if document is not parsable', function() {});
+  });
+  describe('saveProperties()', function() {
+    xit('should stringify whatever props are sent to it', function() {});
+    xit('should fail gracefully if properties cannot be serialized', function() {});
+    xit('should update file with stringified props', function() {});
   });
 });
