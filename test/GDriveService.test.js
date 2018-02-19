c3b1e53ce851d6403ef2fd4d8093eddaf44c0685
Eric <eric@ericyd.com>
2018-02-18 21:35:43 -0800

    add GDriveService tests

--- test/GDriveService.test.js
+++ test/GDriveService.test.js
@@ -1,9 +1,29 @@
-var GDriveService = require('../lib/GDriveService');
-var assert = require('assert');
+const GDriveService = require('../lib/GDriveService');
+const assert = require('assert');
+const sinon = require('sinon');
 
-describe('gdriveService', function() {
-  it('should be importable', function() {
-    var service = new GDriveService();
-    assert.equal(service.ss, 'mySpreadsheetInstance');
+describe('GDriveService', function() {
+  describe('insertBlankFile()', function() {
+    it('should insert a file to the folder ID it was called with', function() {
+      const stubInsert = sinon.stub(GDriveService, 'insertFolder');
+      const parentID = '1234345346';
+      const blankFile = GDriveService.insertBlankFile(parentID);
+      const arg = stubInsert.getCall(0).args[0];
+      assert(
+        stubInsert.calledOnce,
+        'GDriveservice.insertFolder not called once'
+      );
+      assert.equal(arg.mimeType, 'text/plain', 'mimetype not text/plain');
+      assert.equal(
+        arg.parents[0].id,
+        parentID,
+        'file inserted with wrong parentID'
+      );
+      assert.notEqual(
+        arg.title.indexOf('DO NOT DELETE OR MODIFY'),
+        -1,
+        "file title does not contain 'DO NOT DELETE OR MODIFY'"
+      );
+    });
   });
 });
