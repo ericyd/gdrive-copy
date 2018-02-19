9aa650974b40e9bbcfab1b2b020b153c58b2b5f7
Eric Dauenhauer <eric@ericyd.com>
2018-02-19 14:36:35 -0800

    write tests for exponentialBackoff()

--- test/Util.test.js
+++ test/Util.test.js
@@ -1,9 +1,55 @@
-var Util = require('../lib/Util');
-var assert = require('assert');
+global.Utilities = require('./mocks/Utilities');
+const Util = require('../lib/Util');
+const sinon = require('sinon');
+const assert = require('assert');
 
 describe('Util', function() {
+  beforeEach(function() {
+    this.clock = sinon.useFakeTimers();
+  })
+  afterEach(function() {
+    this.clock.restore();
+  })
   describe('exponentialBackoff()', function() {
-    xit('should retry x times on failure', function() {});
+    it('should retry 6 times on failure', function() {
+      // set up mocks
+      const errMsg = 'i failed';
+      const failingFunc = sinon.stub().throws(errMsg);
+      const stubLog = sinon.stub(Util, 'log');
+
+      const failingFunc2 = () => {
+        throw new Error(errMsg);
+      }
+
+      // set up actual
+      try {
+        Util.exponentialBackoff(failingFunc, 'failure message');
+      } catch (e) {
+      }
+
+      // assertions
+      assert.equal(failingFunc.callCount, 6, "failing func not called 6 times");
+      assert.equal(stubLog.callCount, 7, "Util.log not called 7 times");
+
+      // reset mocks
+      stubLog.restore();
+    });
+    it('should rethrow error after 6 tries', function() {
+      // set up mocks
+      const errMsg = 'i failed';
+      const stubLog = sinon.stub(Util, 'log');
+      const failingFunc2 = () => {
+        throw new Error(errMsg);
+      }
+
+      // set up actual and assert
+      assert.throws(() => {
+        Util.exponentialBackoff(failingFunc, 'failure message');
+      }, errMsg, "threw wrong error after 6 attempts");
+
+      // reset mocks
+      stubLog.restore();
+    });
   });
   describe('log()', function() {
     xit('should log to spreadsheet', function() {});
--- test/mocks/Utilities.js
+++ test/mocks/Utilities.js
@@ -1,6 +1,16 @@
+function sleepPromise(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const Utilities = {
   newBlob: function(x) {
     return x;
+  },
+  sleep: function(ms) {
+    return sleepPromise(ms).then(_ => false);
+  },
+  formatDate: function(date) {
+    return date.toString();
   }
 };
 
