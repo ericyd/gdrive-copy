cf9751efb3cc8d9a37efdf0c62e98f87125ba580
Eric Dauenhauer <edauenhauer@acorns.com>
2019-02-03 16:16:02 -0800

    use before/after hooks

--- test/public.test.js
+++ test/public.test.js
@@ -8,32 +8,33 @@ import * as fs from 'fs';
 
 describe('getMetadata', () => {
   beforeEach(function() {
-    this.gDriveService = new GDriveService();
+    this.getFileStub = stub(Drive.Files, 'get');
   });
+
+  afterEach(function() {
+    this.getFileStub.restore();
+  });
+
   it('returns file resource', function() {
-    const getFileStub = stub(Drive.Files, 'get');
     const mockCopy = JSON.parse(
       fs.readFileSync('test/mocks/copy_file_response_200.json').toString()
     );
     const mockId = mockCopy.id;
-    getFileStub.withArgs(mockId).returns(mockCopy);
+    this.getFileStub.withArgs(mockId).returns(mockCopy);
     const actual = getMetadata(mockId, 'url');
     assert.deepEqual(actual, mockCopy, 'actual and mockCopy do not match');
-    getFileStub.restore();
   });
 
   it('returns URL in error message', function() {
-    const getFileStub = stub(Drive.Files, 'get');
     const mockId = '123';
     const url = 'https://drive.google.com/?id=test';
     const errorMessage = `Unable to find a folder with the supplied URL. You submitted ${url}. Please verify that you are using a valid folder URL and try again.`;
-    getFileStub.throws(errorMessage);
+    this.getFileStub.throws(errorMessage);
     try {
       const actual = getMetadata(mockId, url);
       assert.equal(actual, errorMessage, 'did not bubble up error');
     } catch (e) {
       assert.equal(e.message, errorMessage, 'does not throw correct error');
     }
-    getFileStub.restore();
   });
 });
